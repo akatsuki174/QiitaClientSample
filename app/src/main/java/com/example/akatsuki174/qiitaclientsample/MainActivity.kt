37e7a1395f5ed7bbb4a0fca17e4439597b50e4e3
akatsuki174 <dev2015js@gmail.com>
2018-01-10 07:53:18 +0900

    画面に表示する処理を追加

--- app/src/main/java/com/example/akatsuki174/qiitaclientsample/MainActivity.kt
+++ app/src/main/java/com/example/akatsuki174/qiitaclientsample/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.akatsuki174.qiitaclientsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.akatsuki174.qiitaclientsample.model.Article
+import com.example.akatsuki174.qiitaclientsample.model.User
+import com.example.akatsuki174.qiitaclientsample.view.ArticleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val articleView = ArticleView(applicationContext)
+        articleView.setArticle(Article(
+                id = "123",
+                title = "Kotlin入門",
+                url = "http://www.example.com/articles/123",
+                user = User("456", "たろう", "")
+        ))
+        setContentView(articleView)
     }
 }
