ade142f7a3f86f8b987527e04ba706433a332f6e
akatsuki174 <dev2015js@gmail.com>
2018-01-14 23:50:07 +0900

    アダプタークラスを追加

---
+++ app/src/main/java/com/example/akatsuki174/qiitaclientsample/ArticleListAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.akatsuki174.qiitaclientsample
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.example.akatsuki174.qiitaclientsample.model.Article
+import com.example.akatsuki174.qiitaclientsample.view.ArticleView
+
+class ArticleListAdapter(private val context: Context): BaseAdapter() {
+    var articles: List<Article> = emptyList()
+
+    override fun getCount(): Int = articles.size
+    override fun getItem(position: Int): Any? = articles[position]
+    override fun getItemId(position: Int): Long = 0
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View
+            = ((convertView as? ArticleView) ?: ArticleView(context)).apply {
+        setArticle(articles[position])
+    }
+}
